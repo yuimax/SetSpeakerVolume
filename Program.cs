0c0fa3f4c33d70777b254a5b9abf7729d044c387
yuimax <yuima@pilikala.net>
2025-05-09 18:25:45 +0900

    Main() を清書した

--- Program.cs
+++ Program.cs
@@ -5,35 +5,46 @@ class SetVol
 {
     static void Main(string[] args)
     {
+        int volumeLevel = 0;
+
         if (args.Length == 0)
         {
-            try
+            ShowVolume();
+        }
+        else if (args.Length != 1
+            || !int.TryParse(args[0], out volumeLevel)
+            || volumeLevel < 0
+            || volumeLevel > 100)
+        {
+            ShowUsage();
+        }
+        else
+        {
+            SetVolume(volumeLevel);
+        }
+    }
+
+    static void ShowVolume()
+    {
+        try
+        {
+            using (var enumerator = new MMDeviceEnumerator())
             {
-                using (var enumerator = new MMDeviceEnumerator())
+                using (var device = enumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia))
                 {
-                    using (var device = enumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia))
-                    {
-                        float volume = device.AudioEndpointVolume.MasterVolumeLevelScalar;
-                        Console.WriteLine($"Volume is now {Math.Round(volume * 100.0f)}%");
-                    }
+                    float volume = device.AudioEndpointVolume.MasterVolumeLevelScalar;
+                    Console.WriteLine($"Volume is now {Math.Round(volume * 100.0f)}%");
                 }
             }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error getting volume: {ex.Message}");
-            }
-            return;
         }
-
-        if (args.Length != 1 || !int.TryParse(args[0], out int volumeLevel) || volumeLevel < 0 || volumeLevel > 100)
+        catch (Exception ex)
         {
-            Console.WriteLine("Usage: setvol { -h | 0...100 }");
-            Console.WriteLine(" -h      : show this Usage");
-            Console.WriteLine(" 0...100 : set volume");
-            Console.WriteLine(" no args : show current volume");
-            return;
+            Console.WriteLine($"Error getting volume: {ex.Message}");
         }
+    }
 
+    static void SetVolume(int volumeLevel)
+    {
         try
         {
             using (var enumerator = new MMDeviceEnumerator())
@@ -50,5 +61,13 @@ class SetVol
         {
             Console.WriteLine($"Error setting volume: {ex.Message}");
         }
+    }
+
+    static void ShowUsage()
+    {
+        Console.WriteLine("Usage: setvol { -h | 0...100 }");
+        Console.WriteLine(" -h      : show this Usage");
+        Console.WriteLine(" 0...100 : set volume");
+        Console.WriteLine(" no args : show current volume");
     }
 }
